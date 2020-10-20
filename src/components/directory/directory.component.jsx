a0d0473a865c60433283d4c0cdcf99c838d2475d
siddharthh98 <siddharthh.gaurr@gmail.com>
2020-10-20 22:16:51 +0530

    broken our homepage into various components and styled the menu-item components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.style.scss';
+
+class Directory extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            sections:[
+                {
+                  title: 'hats',
+                  imageUrl: 'https://images.unsplash.com/photo-1593724707119-304c12f7cf56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&h=380&q=60',
+                  id: 1,
+                  linkUrl: 'shop/hats'
+                },
+                {
+                  title: 'jackets',
+                  imageUrl: 'https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&h=380&q=60',
+                  id: 2,
+                  linkUrl: 'shop/jackets'
+                },
+                {
+                  title: 'sneakers',
+                  imageUrl: 'https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&h=400&q=60',
+                  id: 3,
+                  linkUrl: 'shop/sneakers'
+                },
+                {
+                  title: 'womens',
+                  imageUrl: 'https://images.unsplash.com/photo-1559563458-527698bf5295?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1110&h=500&q=80',
+                  size: 'large',
+                  id: 4,
+                  linkUrl: 'shop/womens'
+                },
+                {
+                  title: 'mens',
+                  imageUrl: 'https://images.unsplash.com/photo-1553091730-9c2e2b50740a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1110&h=500q=60',
+                  size: 'large',
+                  id: 5,
+                  linkUrl: 'shop/mens'
+                }
+              ] 
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({title, imageUrl, id, size}) => (
+                        <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size}/>
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default Directory;
