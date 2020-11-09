5db017b92d423ac20cb563d1a9c432fcb0a3c495
siddharthh98 <siddharthh.gaurr@gmail.com>
2020-11-09 11:03:25 +0530

    refine code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './components/shop/shop.component'; 
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import './App.css';
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path='/' component={ HomePage }></Route>
-        <Route exact path = '/shop' component = { ShopPage }></Route>
+        <Route path = '/shop' component = { ShopPage }></Route>
+        <Route path = '/signin' component = { SignInAndSignUpPage }></Route>
       </Switch>
     </div>
   );
